2432e4decfe382528b55620b5c0dc02efbc849f3
youngwind <540506594@qq.com>
2016-04-09 17:39:04 +0800

    区分添加成功和失败两种状态

--- popup.js
+++ popup.js
@@ -12,12 +12,13 @@ chrome.tabs.query({
 
 function StarThisRep(res) {
   var starTip = document.querySelector('#star_tip');
+  var inputLabelContainer = document.querySelector('#input-label-container');
   if (!res) {
     starTip.textContent = '仓库不能重复star';
   } else {
     starTip.textContent = res + ' star成功！';
+    inputLabelContainer.style.display = 'block';
   }
-
 }
 
 
